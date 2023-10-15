02e2c2d532b915eef2d4c7eb0574ae93dd5897ab
Yurve <https://github.com/Yurve>
2023-10-15 20:45:46 +0900

    model load time test

--- WebServer/wwwroot/tflite/load_tflite.js
+++ WebServer/wwwroot/tflite/load_tflite.js
@@ -23,5 +23,9 @@ async function inference(input, model_name) {
 
 
 async function initializeModel(model_name) {
+    // 시간 체크 
+    const t0 = performance.now();
     model = await tflite.loadTFLiteModel("/models/yolov8n_" + model_name + "_224.tflite", { numThreads: navigator.hardwareConcurrency / 2 });  
+    const t1 = performance.now();
+    console.log("Model Load Time: " + (t1 - t0) + " milliseconds.");
 }
