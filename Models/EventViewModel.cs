8d243ad1af2b038be263d3d3a059ba606947ca86
SeokHyun Youn <cheongyeon2018@gmail.com>
2023-10-26 03:11:23 +0900

    .

--- Models/EventViewModel.cs
+++ Models/EventViewModel.cs
@@ -9,7 +9,7 @@ namespace Models
 	public class EventViewModel
 	{
         public int Id { get; set; }
-        public string? Date { get; set; }
+        public DateTime? Date { get; set; }
 		public string? CameraName { get; set; }
 		public string? Labels { get; set; }
 		public string? Path { get; set; }
